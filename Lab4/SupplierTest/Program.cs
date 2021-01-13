c4453a9b87a4305af2c365e9cb0bd54470d2b838
MaiPhan20 <65001496+MaiPhan20@users.noreply.github.com>
2021-01-13 17:39:04 +0700

    Lab4

--- Lab4/SupplierTest/Program.cs
+++ Lab4/SupplierTest/Program.cs
@@ -1,37 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Supplier
+namespace SupplierTest
 {
-    class Inventory
+    class Program
     {
-        private int _supplierID;
-        private string _supplierName;
-        private string _city;
-        private string _phoneNo;
-        private string _email;
-        internal void AcceptDetails()
+        static void Main(string[] args)
         {
-            Console.Write("Enter the ID of supplier: ");
-            _supplierID = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the name of supplier:");
-            _supplierName = Console.ReadLine();
-            Console.Write("Enter the city of supplier: ");
-            _city = Console.ReadLine();
-            Console.Write("Enter phone No.: ");
-            _phoneNo = Console.ReadLine();
-            Console.Write("Enter email address: ");
-            _email = Console.ReadLine();
-        }
-        public void DisplayDetails()
-        {
-            Console.WriteLine("\nSupplier Details:");
-            Console.WriteLine("Supplier ID: " + _supplierID);
-            Console.WriteLine("Supplier Name: " + _supplierName);
-            Console.WriteLine("City: " + _city);
-            Console.WriteLine("Phone No: " + _phoneNo);
-            Console.WriteLine("Email: " + _email);
+            Console.WriteLine("Hello World!");
         }
     }
 }
